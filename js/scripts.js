3bb2a8c8a29c60530a06cdd26872679b87fa1f72
Helena Lopez Lara <36647785+AtomicLena@users.noreply.github.com>
2021-12-05 21:36:40 -0600

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,44 @@
 var earth;
 
-async function geoApi()
-{
+async function geoApi() {
     const response = await fetch('api-ip.php', {
         method: 'GET'
     });
     return response.text();
 }
 
-function geolocation()
-{
+function geolocation() {
     geoApi()
-    .then( data => {
-        console.log(data);
-        let geoInfo = '<h1>Geolocalización</h1>'+data;
-        document.getElementById('respuesta').innerHTML = geoInfo;
+        .then(data => {
+            console.log(data);
+            let geoInfo = '<h1>Geolocation</h1>' + data;
+            document.getElementById('respuesta').innerHTML = geoInfo;
 
-        //se rompe a no ser que devuelvas un JSON
-        earth.panTo([data.results.latitude,data.results.longitude]);
-    })
-    .catch( () => {
-        console.log("error");
-    })
+            //se rompe a no ser que devuelvas un JSON
+            earth.panTo([data.results.latitude, data.results.longitude]);
+        })
+        .catch(() => {
+            console.log("error");
+        })
 }
 
 initialize()
-.then( () => {
-    geolocation();
-})
-.catch( () => {
-    console.log("error 2");
-});
+    .then(() => {
+        geolocation();
+    })
+    .catch(() => {
+        console.log("error 2");
+    });
 
-async function initialize()
-{
+async function initialize() {
     earth = WE.map('earth_div', {
         center: [0, 0],
         zoom: 3,
         dragging: false,
         scrollWheelZoom: true
-      });
-    WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-      attribution: '© OpenStreetMap contributors'
+    });
+    WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
     }).addTo(earth);
 
     return Promise.resolve();
